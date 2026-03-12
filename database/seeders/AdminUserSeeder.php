efb9bf45337257e59b8a78914f4ed8892e6f4a3d
SengHour2387 <your-email@example.com>
2026-03-13 00:54:23 +0700

    Change email

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,7 +11,7 @@ class AdminUserSeeder extends Seeder
     public function run(): void
     {
             User::updateOrCreate(
-                ['email' => 'admin@electricstore.com'],
+                ['email' => 'kongbootang@gmail.com'],
                 [
                     'name' => 'System Admin',
                     'password' => Hash::make('password123'),
